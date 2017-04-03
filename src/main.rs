d271f8085d79928f91f91af242324fda8e4db4f9
Maxim Shishmarev <maxim.shishmarev@monash.edu>
2017-04-03 16:07:02 +1000

    specifying path + some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::Path;
 use std::net::{TcpStream};
 
 
 fn main() {
 
-    let path = Path::new("/home/maxim/phd/rust-reader/foo.txt");
-    
-    let mut file = File::open(path).unwrap();
+    let args : Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+      println!("Usage: .\\rust-reader <path>");
+      std::process::exit(0);
+    }
+
+    let path = args[1].clone();
+
+    let mut file = match File::open(path) {
+      Ok(v) => v,
+      Err(e) => {
+        println!("{:?}", e);
+        std::process::exit(0);
+      }
+
+    };
 
     let mut buf: Vec<u8> = Vec::new();
+    file.read_to_end(&mut buf).unwrap();
 
-    file.read_to_end(&mut buf);
+    let mut socket = match TcpStream::connect("127.0.0.1:6565") {
+      Ok(v) => v,
+      Err(e) => {
+        if e.kind() == std::io::ErrorKind::ConnectionRefused {
+          println!("Connection refused. Is profiler running?");
+        } else {
+          println!("{:?}", e.kind());
+        }
+        std::process::exit(0);
+      }
+    };
 
-    let mut socket = TcpStream::connect("127.0.0.1:6565").unwrap();
-
-    socket.write(&buf);
+    /// Can't I just stream from a file to a socket?
+    socket.write(&buf).unwrap();
 }
