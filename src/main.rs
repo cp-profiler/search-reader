a8ec6d2fdb2119639d25636ea573ae52fc58bac3
Maxim Shishmarev <maxim.shishmarev@monash.edu>
2017-05-07 15:11:31 +1000

    added 'debug' option

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,99 @@
+extern crate clap;
+extern crate byteorder;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::{TcpStream};
 
+use clap::Arg;
+
+use byteorder::{LittleEndian, ByteOrder};
+
+fn read_file(file: &mut File, debug: bool) {
+  let mut socket = match TcpStream::connect("127.0.0.1:6565") {
+    Ok(v) => v,
+    Err(e) => {
+      if e.kind() == std::io::ErrorKind::ConnectionRefused {
+        println!("Connection refused. Is profiler running?");
+      } else {
+        println!("{:?}", e.kind());
+      }
+      std::process::exit(0);
+    }
+  };
+
+  let mut buf = [0; 500];
+
+  loop {
+
+    if debug {
+      println!("Press <ENTER> to send next node\n");
+      let mut line = String::new();
+      std::io::stdin().read_line(&mut line);
+    }
+
+    let msg_len = {
+      let mut buf = [0; 4];
+      match file.read_exact(&mut buf) {
+        Ok(()) => (),
+        Err(_) => { break; }
+      }
+      socket.write(&buf).unwrap();
+      LittleEndian::read_u32(&buf)
+    };
+
+    let mut buf = {
+      let mut buf: Vec<u8> = Vec::new();
+      buf.resize(msg_len as usize, 0);
+      buf.into_boxed_slice()
+    };
+
+    match file.read_exact(&mut buf) {
+      Ok(()) => (),
+      Err(_) => { break; }
+    };
+
+    let size = match socket.write(&buf) {
+      Ok(v) => v,
+      Err(_) => 0
+    };
+  }
+}
 
 fn main() {
 
-    let args : Vec<String> = std::env::args().collect();
+  let args = clap::App::new("Search Reader")
+    .version("0.1")
+    .about("Reads nodes from a file and sends to CP-Profiler.")
+    .author("Maxim Shishmarev")
 
-    if args.len() != 2 {
-      println!("Usage: .\\rust-reader <path>");
-      std::process::exit(0);
-    }
+    .arg(Arg::with_name("file")
+      .help("File to read")
+      .required(true)
+      .index(1))
 
-    let path = args[1].clone();
+    .arg(Arg::with_name("debug")
+      .short("d")
+      .long("debug")
+      .help("send nodes one-by-one. (unimplemented)")
+      .takes_value(false))
 
-    let mut file = match File::open(path) {
+    .get_matches();
+
+    let path = args.value_of("file").unwrap();
+
+    let mut file = match File::open(&path) {
       Ok(v) => v,
       Err(e) => {
         println!("{:?}", e);
+        println!("File location: {:?}", &path);
         std::process::exit(0);
       }
 
     };
 
-    let mut buf: Vec<u8> = Vec::new();
-    file.read_to_end(&mut buf).unwrap();
+    let debug = args.is_present("debug");
 
-    let mut socket = match TcpStream::connect("127.0.0.1:6565") {
-      Ok(v) => v,
-      Err(e) => {
-        if e.kind() == std::io::ErrorKind::ConnectionRefused {
-          println!("Connection refused. Is profiler running?");
-        } else {
-          println!("{:?}", e.kind());
-        }
-        std::process::exit(0);
-      }
-    };
+    read_file(&mut file, debug);
 
-    /// Can't I just stream from a file to a socket?
-    socket.write(&buf).unwrap();
 }
